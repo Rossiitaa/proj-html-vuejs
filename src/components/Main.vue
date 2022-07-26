70f473607b8a934804406322d80df31b39ab1c23
Rossiitaa <rosario.scuotto@outlook.it>
2022-07-26 17:41:19 +0200

    countdown section with data completed (to be improved)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,7 +2,7 @@
     <main>
         <div class="container">
             <!-- informazioni azienda -->
-            <div id="info-all" class="row d-flex justify-content-evenly align-items-center mx-auto">
+            <div id="info-general" class="row d-flex justify-content-evenly align-items-center mx-auto">
                 <div class="info-text col-12 d-flex flex-column justify-content-around" v-for="(info , index) in informations" :key="index">
                     <div class="d-flex fs-4">
                         <font-awesome-icon :icon="['fas', info.icon]" class="mr-3" />
@@ -17,6 +17,35 @@
                 </div>
             </div>
         </div>
+            <!-- countdown -->
+        <div class="container-fluid">
+            <div id="countdown" class="row d-flex align-items-center">
+                <div id="count-card" class="d-flex flex-column justify-content-between">
+                    <div class=" d-flex justify-content-center">
+                        <div class="card align-items-center py-4 px-5 mx-2"
+                        v-for="(count, index) in countdown" :key="index" :style="`background: ${count.bg}; opacity: ${count.transp}`">
+                            <h1>
+                                {{ count.number }}
+                            </h1>
+                            <span>
+                                {{ count.text }}
+                            </span>
+                        </div>
+                    </div>
+
+                    <h5 class="fw-bold">
+                        SUBSCRIBE FOR UPDATES
+                    </h5>
+                    <div id="update">
+                        <input type="text" placeholder="Please fill your email"
+                        class="p-3 mx-3">
+                        <button class="p-3 px-4">
+                            SUBSCRIBE
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
@@ -26,33 +55,60 @@ data() {
     return {
         informations: [
             {
-                icon: " fa-trophy",
+                icon: "fas fa-trophy",
                 title: "WHO WE ARE",
                 text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
                 url: "#",
                 urlText: "Learn More"
             },
             {
-                icon: " fa-bolt",
+                icon: "fas fa-bolt",
                 title: "WHAT WE DO",
                 text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
                 url: "#",
                 urlText: "Learn More"
             },
             {
-                icon: " fa-question",
+                icon: "fas fa-question",
                 title: "WHY US?",
                 text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
                 url: "#",
                 urlText: "Learn More"
             },
-        ]}
+        ],
+        countdown: [
+            {
+                number: "3399",
+                text: "DAYS",
+                bg: "@000",
+                transp: "0.8"
+            },
+            {
+                number: "17",
+                text: "HOURS",
+                bg: "@000",
+                transp: "0.7"
+            },
+            {
+                number: "28",
+                text: "MINS",
+                bg: "@000",
+                transp: "0.5"
+            },
+            {
+                number: "17",
+                text: "SECS",
+                bg: "@000",
+                transp: "0.3"
+            },
+        ]
+        }
     }
 }
 </script>
 
 <style lang='scss' scoped>
-#info-all {
+#info-general {
     width: 60%;
     height: 30vh;
 
@@ -67,11 +123,52 @@ data() {
         }
         p {
             font-size: 0.8rem;
-            color: #878787;
+            color: #868686;
         }
         a {
             color: #f72b0c;
             font-style: italic;
         }
+    }
+
+#countdown {
+    height: 50vh;
+    background: url(../assets/images/coutdown-bg1.jpg);
+    background-size: contain;
+    background-position: center;
+    #count-card {
+        height: 70%;
+            h1 {
+                font-size: 50px;
+                font-weight: 700;
+            }
+            .card {
+                background-color: #000;
+                color: #fff;
+            }
+        }
+        #update {
+            font-size: 13px;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            
+            input {
+                outline: 0;
+                border: 0;
+                width: 22%;
+            }
+            button {
+                font-weight: 700;
+                border-width: 0 0 3px 0;
+                border-color: #d92719;
+                background: #f72b0c;
+                color: #fff;
+            }
+        }
+        h5 {
+                display: flex;
+                justify-content: center;
+            }
     }
 </style>
