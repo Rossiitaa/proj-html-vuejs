6625b33b23218968a77965dfa2cf38abc7724ffd
Rossiitaa <rosario.scuotto@outlook.it>
2022-07-26 12:37:04 +0200

    created components and added in app + importe bootstrap

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <div class="container">
+            <nav>
+                
+            </nav>
+        </div>
+    </header>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
