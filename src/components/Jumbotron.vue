9dc79c50c2cbdc5e815ad0ac995b05c6d688906f
Rossiitaa <rosario.scuotto@outlook.it>
2022-07-26 15:07:07 +0200

    Jumbotron complete

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -3,7 +3,9 @@
         <img src="../assets/images/slider-1-bg.jpg" alt="">
         <h2>CONFERENCE ON</h2>
         <h1>SOCIAL NETWORKS</h1>
-        <img class="position-absolute" src="../assets/images/1-item-2.jpg" alt="">
+        <img class="line" src="../assets/images/1-item-2.jpg" alt="">
+        <p class="">23-26 MAY 2019 <span class="mx-4 fw-light">/</span> SANS FRANCISCO <span class="mx-4 fw-light">/</span> 8 SPEAKERS</p>
+        <button class="btn">REGISTER ONLINE</button>
     </div>
 </template>
 
@@ -15,7 +17,7 @@ export default {
 
 <style lang='scss' scoped>
 .cont-jumb {
-        height: 36rem;
+        height: 40rem;
     }
 
 img {
@@ -32,16 +34,47 @@ img {
         font-size: 4rem;
         font-weight: lighter;
         color: #fff;
-        text-shadow: 0 0 10px #000;
     }
     h1 {
         position: absolute;
-        top: 45%;
+        top: 40%;
         left: 50%;
         transform: translate(-50%, -50%);
-        font-size: 5rem;
+        font-size: 6rem;
         font-weight: bold;
         color: #fff;
-        text-shadow: 0 0 10px #000;
+    }
+    img.line {
+        position: absolute;
+        top: 58%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 35rem;
+        height: 0.3rem;
+    }
+    p {
+        position: absolute;
+        top: 62%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 1.2rem;
+        font-weight: bold;
+        color: #fff;
+    }
+    button {
+        position: absolute;
+        top: 75%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 1.2rem;
+        font-weight: bold;
+        color: #fff;
+        padding: 1rem 2rem;
+        background-color: #f72a0c;
+
+        &:hover {
+            background-color: #f72a0c;
+            color: #fff;
+        }
     }
 </style>
